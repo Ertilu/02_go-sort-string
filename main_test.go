c3ba42efedbbf16fa6ee47ee5fd533d476ec93ad
Reza Raka Nugraha <53091940+Ertilu@users.noreply.github.com>
2019-10-15 21:56:16 +0700

    Add benchmark
    Bug fixes for testing

--- main_test.go
+++ main_test.go
@@ -14,53 +14,6 @@ func TestSort(t *testing.T) {
 			t.Fatalf("return must be %s instead %s", expectedResult, sort)
 		}
 	})
-}
-package main
-
-import (
-	"testing"
-)
-
-func TestSort(t *testing.T) {
-	// test is the input is nil
-	t.Run("Nil input", func(t *testing.T) {
-		input := ""
-		sort := sortVowels(input)
-		expectedResult := "Hasil sorting: "
-		if sort != expectedResult {
-			t.Fatalf("return must be %s instead %s", expectedResult, sort)
-		}
-	})
-
-	// test is the are the input of string
-	t.Run("Input osama string", func(t *testing.T) {
-		input := "osama"
-		sort := sortVowels(input)
-		expectedResult := "Hasil sorting: aaoms"
-		if sort != expectedResult {
-			t.Fatalf("return must be %s instead %s", expectedResult, sort)
-		}
-	})
-
-	// call the main function so the coverage will 100%
-	main()
-}
-		package main
-
-import (
-	"testing"
-)
-
-func TestSort(t *testing.T) {
-	// test is the input is nil
-	t.Run("Nil input", func(t *testing.T) {
-		input := ""
-		sort := sortVowels(input)
-		expectedResult := "Hasil sorting: "
-		if sort != expectedResult {
-			t.Fatalf("return must be %s instead %s", expectedResult, sort)
-		}
-	})
 
 	// test is the are the input of string
 	t.Run("Input osama string", func(t *testing.T) {
