b2ae613f47f4015139fd8e7387d8b06fc85c9dc5
Reza Raka Nugraha <53091940+Ertilu@users.noreply.github.com>
2019-10-15 21:52:47 +0700

    Add benchmark

--- main_test.go
+++ main_test.go
@@ -45,4 +45,40 @@ func TestSort(t *testing.T) {
 	// call the main function so the coverage will 100%
 	main()
 }
-		
+		package main
+
+import (
+	"testing"
+)
+
+func TestSort(t *testing.T) {
+	// test is the input is nil
+	t.Run("Nil input", func(t *testing.T) {
+		input := ""
+		sort := sortVowels(input)
+		expectedResult := "Hasil sorting: "
+		if sort != expectedResult {
+			t.Fatalf("return must be %s instead %s", expectedResult, sort)
+		}
+	})
+
+	// test is the are the input of string
+	t.Run("Input osama string", func(t *testing.T) {
+		input := "osama"
+		sort := sortVowels(input)
+		expectedResult := "Hasil sorting: aaoms"
+		if sort != expectedResult {
+			t.Fatalf("return must be %s instead %s", expectedResult, sort)
+		}
+	})
+
+	// call the main function so the coverage will 100%
+	main()
+}
+
+func BenchmarkSortVowels(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		input := "aiueo"
+		sortVowels(input)
+	}
+}
