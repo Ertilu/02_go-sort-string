2471d0dbcaf0b1a1600b1f2c150ce654cd13b5f2
Reza Raka Nugraha <53091940+Ertilu@users.noreply.github.com>
2019-10-15 21:50:30 +0700

    Update testing 100% covered main.go file

--- main_test.go
+++ main_test.go
@@ -15,3 +15,34 @@ func TestSort(t *testing.T) {
 		}
 	})
 }
+package main
+
+import (
+	"testing"
+)
+
+func TestSort(t *testing.T) {
+	// test is the input is nil
+	t.Run("Nil input", func(t *testing.T) {
+		input := ""
+		sort := sortVowels(input)
+		expectedResult := "Hasil sorting: "
+		if sort != expectedResult {
+			t.Fatalf("return must be %s instead %s", expectedResult, sort)
+		}
+	})
+
+	// test is the are the input of string
+	t.Run("Input osama string", func(t *testing.T) {
+		input := "osama"
+		sort := sortVowels(input)
+		expectedResult := "Hasil sorting: aaoms"
+		if sort != expectedResult {
+			t.Fatalf("return must be %s instead %s", expectedResult, sort)
+		}
+	})
+
+	// call the main function so the coverage will 100%
+	main()
+}
+		
